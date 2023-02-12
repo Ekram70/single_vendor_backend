cc95f374531fe594c31fb8cdf6fb0285dfcb6ae5
Ekram70 <ekramullah70@gmail.com>
2023-02-12 13:37:19 +0600

    chore: eslint prettier problem fixed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:3500', 'http://localhost:5173', process.env.ORIGIN];
+const allowedOrigins = ['http://localhost:3500', 'http://localhost:5173',"http://127.0.0.1:5173", process.env.ORIGIN];
 
 module.exports = allowedOrigins;
