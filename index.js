9233be4816cc7184935ac08cacd616e6ebb746ea
Ekram70 <ekramullah70@gmail.com>
2023-02-15 14:45:14 +0600

    feat: compression, sharp and multer for uploading and compression

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const compression = require("compression");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
+const fsPromises = require("fs").promises;
+const sharp = require("sharp");
 require("dotenv").config();
 
 const { logAccessToFile, logToConsole } = require("./src/middlewares/logger");
@@ -22,6 +26,8 @@ const logoutRouter = require("./src/routes/logoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3500;
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // parser
 app.use(express.urlencoded({ extended: true }));
@@ -37,12 +43,74 @@ app.disable("x-powered-by");
 app.use(mongoSanitize());
 app.use(xss());
 app.use(hpp());
+
 // logger
 app.use(logAccessToFile);
 app.use(logToConsole);
 
+// compress all responses
+app.use(compression());
+
+// static folder
+app.use(express.static("./uploads"));
+
 // Connect to MongoDB
 connectDB();
+
+// testing
+app.post("/", upload.single("picture"), async (req, res) => {
+  try {
+    await fsPromises.access("./uploads");
+  } catch (error) {
+    try {
+      await fsPromises.mkdir("./uploads");
+    } catch (err) {
+      res.status(500).json({
+        status: "fail",
+        data: err,
+      });
+    }
+  }
+
+  const { buffer, originalname, mimetype } = req.file;
+  const timestamp = Date.now();
+
+  console.log(mimetype);
+
+  if (
+    !(
+      mimetype === "image/png" ||
+      mimetype === "image/jpg" ||
+      mimetype === "image/jpeg"
+    )
+  ) {
+    res.status(400).json({
+      status: "fail",
+      data: "'Only .png, .jpg and .jpeg format allowed!'",
+    });
+  }
+
+  const name = originalname
+    .substring(0, originalname.lastIndexOf("."))
+    .toLowerCase()
+    .split(" ")
+    .join("-");
+
+  const ref = `${name}-${timestamp}.webp`;
+
+  try {
+    await sharp(buffer).webp({ quality: 100 }).toFile(`./uploads/${ref}`);
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      data: error,
+    });
+  }
+
+  const link = `${process.env.ORIGIN}/${ref}`;
+
+  res.status(200).json({ status: "success", link });
+});
 
 // routing
 app.use("/api/v1/auth", authRouter);
